ff44677812a670ed687a5671d9e5301cd0d9855b
Weibang Jiang <38138106+935963004@users.noreply.github.com>
2024-02-19 10:16:56 +0800

    Update make_h5dataset_for_pretrain.py

--- dataset_maker/make_h5dataset_for_pretrain.py
+++ dataset_maker/make_h5dataset_for_pretrain.py
@@ -6,12 +6,12 @@ savePath = Path('path/to/your/save/path')
 rawDataPath = Path('path/to/your/raw/data/path')
 group = rawDataPath.glob('*.cnt')
 
-# 预处理参数
+# preprocessing parameters
 l_freq = 0.1
 h_freq = 75.0
 rsfreq = 200
 
-#chunks参数, 1s 数据
+# channel number * rsfreq
 chunks = (62, rsfreq)
 
 dataset = h5Dataset(savePath, 'dataset')
@@ -23,7 +23,7 @@ for cntFile in group:
     grp = dataset.addGroup(grpName=cntFile.stem)
     dset = dataset.addDataset(grp, 'eeg', eegData, chunks)
 
-    # dataset属性，预处理信息
+    # dataset attributes
     dataset.addAttributes(dset, 'lFreq', l_freq)
     dataset.addAttributes(dset, 'hFreq', h_freq)
     dataset.addAttributes(dset, 'rsFreq', rsfreq)
